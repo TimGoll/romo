f0391940daed3ecfaea1c6da3f76988b46823f32
Tim Goll <tim@timgoll.de>
2020-06-06 17:40:36 +0200

    GetAng change

--- part_a/solution/GetAng.m
+++ part_a/solution/GetAng.m
@@ -1,3 +1,3 @@
 function [C] = GetAng(A)
-    C = [rad2deg(atan2(A(2,1),A(1,1))) rad2deg(asin(-A(3,1))) rad2deg(atan2(A(3,2),A(3,3)))];
+    C = [atan2(A(2,1),A(1,1)) asin(-A(3,1)) atan2(A(3,2),A(3,3))];
 end
