23f1aaba5b5dccef649ef4ad83bc8d4ea5fbc41d
Tim Goll <tim@timgoll.de>
2020-06-06 17:48:07 +0200

    switch to DEG

--- part_a/solution/GetAng.m
+++ part_a/solution/GetAng.m
@@ -1,3 +1,7 @@
 function [C] = GetAng(A)
-    C = [rad2deg(atan2(A(2,1),A(1,1))) rad2deg(asin(-A(3,1))) rad2deg(atan2(A(3,2),A(3,3)))];
+    C = [
+        rad2deg(atan2(deg2rad(A(2,1)), deg2rad(A(1,1))))
+        rad2deg(asin(deg2rad(-A(3,1))))
+        rad2deg(atan2(deg2rad(A(3,2)), deg2rad(A(3,3))))
+    ];
 end
--- part_a/solution/TransMatrix.m
+++ part_a/solution/TransMatrix.m
@@ -1,8 +1,8 @@
 function [A] = TransMatrix(j, ang, tbl)
     A = [
-        [cos(ang + tbl(j, 1))  -sin(ang + tbl(j, 1))*cos(tbl(j, 4))  sin(ang + tbl(j, 1))*sin(tbl(j, 4))   tbl(j, 3)*cos(ang + tbl(j, 1))];
-        [sin(ang + tbl(j, 1))  cos(ang + tbl(j, 1))*cos(tbl(j, 4))   -cos(ang + tbl(j, 1))*sin(tbl(j, 4))  tbl(j, 3)*sin(ang + tbl(j, 1))];
-        [0                     sin(tbl(j, 4))                        cos(tbl(j, 4))                        tbl(j, 2)                     ];
-        [0                     0                                     0                                     1                             ];
+        [cosd(ang + tbl(j, 1))  -sind(ang + tbl(j, 1))*cosd(tbl(j, 4))  sind(ang + tbl(j, 1))*sind(tbl(j, 4))   tbl(j, 3)*cosd(ang + tbl(j, 1))];
+        [sind(ang + tbl(j, 1))  cosd(ang + tbl(j, 1))*cosd(tbl(j, 4))   -cosd(ang + tbl(j, 1))*sind(tbl(j, 4))  tbl(j, 3)*sind(ang + tbl(j, 1))];
+        [0                      sind(tbl(j, 4))                         cosd(tbl(j, 4))                         tbl(j, 2)                      ];
+        [0                      0                                      0                                        1                              ];
     ];
 end
