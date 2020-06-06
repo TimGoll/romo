fa023ae12bbc04e70d26d774080bf3b7e4f2b45e
Tim Goll <tim@timgoll.de>
2020-06-06 19:19:16 +0200

    Update backward.m

--- part_a/solution/backward.m
+++ part_a/solution/backward.m
@@ -1,23 +1,27 @@
 tbl = [
-    [0 128     0      pi/2 ];   %1
-    [0 0       -612   0    ];   %2
-    [0 0       -572.3 0    ];   %3
-    [0 163.941 0      pi/2 ];   %4
-    [0 115.7   0      -pi/2];   %5
-    [0 92.2    0      0    ];   %6
+    [0 128     0      90 ];   %1
+    [0 0       -612   0  ];   %2
+    [0 0       -572.3 0  ];   %3
+    [0 163.941 0      90 ];   %4
+    [0 115.7   0      -90];   %5
+    [0 92.2    0      0  ];   %6
 ];
 
 wtcp = [-471 -782.73 201.03 -179.88 -24.48 -158];
 
 q = [0 0 0 0 0 0];
 
+UpdateQ(q, wtcp);
+
+return;
+
 while(true)
-    A01 = TransMatrix(1, deg2rad(q(1)), tbl);
-    A12 = TransMatrix(2, deg2rad(q(2)), tbl);
-    A23 = TransMatrix(3, deg2rad(q(3)), tbl);
-    A34 = TransMatrix(4, deg2rad(q(4)), tbl);
-    A45 = TransMatrix(5, deg2rad(q(5)), tbl);
-    A56 = TransMatrix(6, deg2rad(q(6)), tbl);
+    A01 = TransMatrix(1, q(1), tbl);
+    A12 = TransMatrix(2, q(2), tbl);
+    A23 = TransMatrix(3, q(3), tbl);
+    A34 = TransMatrix(4, q(4), tbl);
+    A45 = TransMatrix(5, q(5), tbl);
+    A56 = TransMatrix(6, q(6), tbl);
 
     A06 = A01 * A12 * A23 * A34 * A45 * A56;
 
@@ -26,6 +30,10 @@ while(true)
     if ShouldStop(wtcp_new - wtcp)
         break;
     end
+    
+    q = UpdateQ(q, (wtcp_new - wtcp));
+    
+    break;
 end
 
 disp("Position:")
@@ -48,3 +56,21 @@ function [cancel] = ShouldStop(error)
     cancel = true;
 end
 
+function [qNew] = UpdateQ(q, dif)
+    syms q1 q2 q3 q4 q5 q6;
+    fq = [ 
+        [1 0 0 0 0         0                ];
+        [0 1 0 0 0         0                ];
+        [0 0 1 0 0         0                ];
+        [0 0 0 0 -sind(q6) cosd(q6)*cosd(q5)];
+        [0 0 0 0 cosd(q6)  sind(q6)*cosd(q5)];
+        [0 0 0 1 0         -sind(q5)        ];
+    ] * dif';
+
+    disp(fq);
+
+    dfq = jacobian(fq, [q1, q2, q3, q4, q5, q6]);
+    
+    disp(dfq);
+    disp(inv(dfq));
+end
