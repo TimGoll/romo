2934ab3d43501a37af266cf5646f8aa09ed73a3b
Empijay <48317678+Empijay@users.noreply.github.com>
2020-07-17 12:16:14 +0200

    Revert "Revert "Revert "Simulation (unfinished) added"""
    This reverts commit 913aa6b599f81856579cb05306f644393956cfd5.

--- part_c/solution/test.m
+++ part_c/solution/test.m
@@ -1,17 +1,6 @@
-Wstart = [-1180 -260 10 90 0 0].';
-Wend = [-471 -782.73 201.03 -179.88 -24.48 -158].';
-Qstart = [RandRange(1,361), RandRange(1,361), RandRange(1,361), RandRange(1,361), RandRange(1,361), RandRange(1,361)].';
+p_data = [-471 -782.73 201.03 -179.88 -24.48 -158].';
 
-% Qstart = backward(Wstart,Qstart);
-n = 1000;
-x = linspace(0,1,n);
-A2 = Wstart + x.*(Wend - Wstart);
+q_data = backward(p_data);
 
-    for i = 1:1:n
-        P = A2(:,i);
-        Q = backward(P,Qstart);
-        roboplot(Q);
-        drawnow;
-        Qstart=Q;
-        
-    end
+roboplot(q_data);
+hold on
